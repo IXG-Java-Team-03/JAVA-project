6599a263c990ed0ed360f0333308e095a79181a3
Nick Kotsonis <nkot@intracom-telecom.com>
2018-10-17 17:46:34 +0300

    new behavior of CharContainer and new test cases

--- src/application/CharContainer.java
+++ src/application/CharContainer.java
@@ -3,6 +3,8 @@
  */
 package application;
 
+import java.util.BitSet;
+
 /**
  * @author nkot
  *
@@ -10,25 +12,30 @@ package application;
 public class CharContainer {
 
 	public static int MAX_CHARACTERS = 6;
+	public static char EMPTY_CHAR = 0;
 
 	private final char[] charArray;
-	private int numChars;
+	private final BitSet validLetters;
 	
 	public CharContainer() {
 		charArray = new char[MAX_CHARACTERS];
-		numChars = 0;
+		validLetters = new BitSet();
 	}
 
 	public CharContainer( String letters) {
 		charArray = new char[MAX_CHARACTERS];
+		validLetters = new BitSet();
 		InitLetters( letters);
 	}
 	
 	
 	public void InitLetters(String letters) {
 		
-		numChars = Math.min( MAX_CHARACTERS, letters.length());		// calculate number of characters to set
-		
+		int numChars = Math.min( MAX_CHARACTERS, letters.length());		// calculate number of characters to set
+		validLetters.clear();
+		if( numChars>0) {
+			validLetters.set(0, numChars);
+		}
 		System.arraycopy( letters.toCharArray(), 0,					// source array 
 						  charArray, 0, 							// target array
 						  numChars);								// number of elements
@@ -36,25 +43,41 @@ public class CharContainer {
 
 
 	public void pushLetter(char letter) {
-		if( numChars<MAX_CHARACTERS) {
-			charArray[numChars] = letter;			// Store the character in the array
-			numChars++;								// increase number of characters
+		int insertPosition = validLetters.nextClearBit(0);
+		if( insertPosition < MAX_CHARACTERS) {
+			charArray[insertPosition] = letter;			// Store the character in the array
+			validLetters.set(insertPosition);			// Set the validity of the current letter
 		}
 	}
 
 
 	public char popLetter() {
-		if( numChars>0) {
-			numChars--;								// remove one character
-			return charArray[numChars];				// return that character back
+		int fetchPosition = validLetters.previousSetBit(MAX_CHARACTERS);
+		if( fetchPosition != -1) {
+			char retval = charArray[fetchPosition];
+			charArray[fetchPosition] = EMPTY_CHAR;		// Empty the last position in the array
+			validLetters.clear(fetchPosition);			// remove one character
+			return retval;								// return that character back
 		}
-		return (char)0;
+		return EMPTY_CHAR;
 	}
+	
+	
+	public char popLetter(int position) {
+		if( position < MAX_CHARACTERS && validLetters.get(position)) {
+			char retval = charArray[position];
+			charArray[position] = EMPTY_CHAR;			// Empty the last position in the array
+			validLetters.clear(position);				// remove one character
+			return retval;								// return that character back
+		}
+		return EMPTY_CHAR;
+	}
+	
 	
 	@Override
 	public String toString() {
-		return String.valueOf(charArray)			// char array to String
-				     .substring(0, numChars); 		// limit to number of characters
+		return String.valueOf(charArray)											// char array to String
+				     .substring(0, validLetters.previousSetBit(MAX_CHARACTERS)+1); 	// limit to number of characters
 	}
 
 }
--- src/test/CharContainerTest01.java
+++ src/test/CharContainerTest01.java
@@ -36,7 +36,7 @@ class CharContainerTest01 {
 		String letters = "AR";
 		obj1.InitLetters(letters);
 		String val = obj1.toString();
-		assertEquals( val, letters);
+		assertEquals( letters, val);
 	}
 
 	@Test
@@ -44,7 +44,7 @@ class CharContainerTest01 {
 		String letters = "ARerA";
 		obj1.InitLetters(letters);
 		String val = obj1.toString();
-		assertEquals( val, letters);
+		assertEquals( letters, val);
 	}
 
 	@Test
@@ -54,7 +54,7 @@ class CharContainerTest01 {
 			obj1.pushLetter( letters.charAt(i));
 		}
 		String val1 = obj1.toString();
-		assertEquals( val1, letters);
+		assertEquals( letters, val1);
 	}
 
 	@Test
@@ -65,7 +65,7 @@ class CharContainerTest01 {
 		for( int i=0; i < letters.length(); i++) {
 			val1 = obj1.popLetter() + val1;
 		}
-		assertEquals( val1, letters);
+		assertEquals( letters, val1);
 	}
 
 	@Test
@@ -74,7 +74,7 @@ class CharContainerTest01 {
 		CharContainer obj2 = new CharContainer(letters);
 		String val = obj2.toString();
 		obj2 = null;
-		assertEquals( val, letters);
+		assertEquals( letters, val);
 	}
 
 	@Test
@@ -85,28 +85,91 @@ class CharContainerTest01 {
 		String val1 = "";
 		for( int i=0; i < letters.length()+1; i++) {
 			char t = obj1.popLetter();
-			if( t==0) {
+			if( t == CharContainer.EMPTY_CHAR) {
 				break;
 			}
 			val1 = t + val1;
 		}
-		assertEquals( val1, letters);
+		assertEquals( letters, val1);
 	}
 
 	@Test
 	void test07() {
-		String letters = "";
+		String letters = "ARerA1";
 		obj1.InitLetters(letters);
-		String val = obj1.toString();
-		assertEquals( val, letters);
+		obj1.pushLetter( 't');
+		obj1.popLetter(2);
+		String expected = "ARrA1";
+		String val1 = "";
+		for( int i=0; i < letters.length()+1; i++) {
+			char t = obj1.popLetter();
+			if( t == CharContainer.EMPTY_CHAR) {
+				break;
+			}
+			val1 = t + val1;
+		}
+		assertEquals( expected, val1);
 	}
 
 	@Test
 	void test08() {
+		String letters = "ARerA1";
+		obj1.InitLetters(letters);
+		obj1.popLetter(2);
+		obj1.pushLetter( 't');
+		String expected = "ARtrA1";
+		String val1 = "";
+		for( int i=0; i < letters.length()+1; i++) {
+			char t = obj1.popLetter();
+			if( t == CharContainer.EMPTY_CHAR) {
+				break;
+			}
+			val1 = t + val1;
+		}
+		assertEquals( expected, val1);
+	}
+
+	@Test
+	void test09() {
+		String letters = "";
+		obj1.InitLetters(letters);
+		String val = obj1.toString();
+		assertEquals( letters, val);
+	}
+
+	@Test
+	void test10() {
 		String letters = "ABCDGFHE";
 		obj1.InitLetters(letters);
 		String val = obj1.toString();
-		assertEquals( val, letters.substring(0, CharContainer.MAX_CHARACTERS));
+		assertEquals( letters.substring(0, CharContainer.MAX_CHARACTERS), val);
+	}
+
+	@Test
+	void test11() {
+		String letters = "ABCDGF";
+		obj1.InitLetters(letters);
+		obj1.popLetter(2);
+		String expected = "AB" + CharContainer.EMPTY_CHAR + "DGF";
+		String val = obj1.toString();
+		assertEquals( expected, val);
+	}
+
+	@Test
+	void test12() {
+		String letters = "ABCDGF";
+		obj1.InitLetters(letters);
+		char c = obj1.popLetter(CharContainer.MAX_CHARACTERS+2);
+		assertEquals( CharContainer.EMPTY_CHAR, c);
+	}
+
+	@Test
+	void test13() {
+		String letters = "ABCDGF";
+		obj1.InitLetters(letters);
+		obj1.popLetter(3);
+		char c = obj1.popLetter(3);
+		assertEquals( CharContainer.EMPTY_CHAR, c);
 	}
 
 }
