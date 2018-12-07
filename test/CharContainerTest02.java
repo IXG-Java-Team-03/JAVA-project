a3c33733240f0cde49c7122443698c28bea6c734
Nick Kotsonis <nkotsonis@gmail.com>
2018-12-07 13:12:50 +0200

    minor changes

--- test/CharContainerTest02.java
+++ test/CharContainerTest02.java
@@ -20,17 +20,17 @@ public class CharContainerTest02 {
 	public void test01() {
 		String letters = "ANARE";
 		obj1.InitLetters(letters);
-		int index = obj1.getLetterIndex( "ARENA " + letters, 0);
+		int index = CharContainer.getLetterIndex( "ARENA " + letters, 0);
 		assertEquals( 0, index);
-		index = obj1.getLetterIndex( "ARENA " + letters, 1);
+		index = CharContainer.getLetterIndex( "ARENA " + letters, 1);
 		assertEquals( 3, index);
-		index = obj1.getLetterIndex( "ARENA " + letters, 2);
+		index = CharContainer.getLetterIndex( "ARENA " + letters, 2);
 		assertEquals( 4, index);
-		index = obj1.getLetterIndex( "ARENA " + letters, 3);
+		index = CharContainer.getLetterIndex( "ARENA " + letters, 3);
 		assertEquals( 1, index);
-		index = obj1.getLetterIndex( "ARENA " + letters, 4);
+		index = CharContainer.getLetterIndex( "ARENA " + letters, 4);
 		assertEquals( 2, index);
-		index = obj1.getLetterIndex( "ARENA " + letters, 5);
+		index = CharContainer.getLetterIndex( "ARENA " + letters, 5);
 		assertEquals( CharContainer.NO_INDEX, index);
 	}
 
@@ -39,17 +39,17 @@ public class CharContainerTest02 {
 	public void test02() {
 		String letters = "ANAREN";
 		obj1.InitLetters(letters);
-		int index = obj1.getLetterIndex( "ARNENA " + letters, 0);
+		int index = CharContainer.getLetterIndex( "ARNENA " + letters, 0);
 		assertEquals( 0, index);
-		index = obj1.getLetterIndex( "ARNENA " + letters, 1);
+		index = CharContainer.getLetterIndex( "ARNENA " + letters, 1);
 		assertEquals( 3, index);
-		index = obj1.getLetterIndex( "ARNENA " + letters, 2);
+		index = CharContainer.getLetterIndex( "ARNENA " + letters, 2);
 		assertEquals( 1, index);
-		index = obj1.getLetterIndex( "ARNENA " + letters, 3);
+		index = CharContainer.getLetterIndex( "ARNENA " + letters, 3);
 		assertEquals( 4, index);
-		index = obj1.getLetterIndex( "ARNENA " + letters, 4);
+		index = CharContainer.getLetterIndex( "ARNENA " + letters, 4);
 		assertEquals( 5, index);
-		index = obj1.getLetterIndex( "ARNENA " + letters, 5);
+		index = CharContainer.getLetterIndex( "ARNENA " + letters, 5);
 		assertEquals( 2, index);
 	}
 
@@ -58,22 +58,22 @@ public class CharContainerTest02 {
 	public void test03() {
 		String letters = "ANARWEN";
 		obj1.InitLetters(letters);
-		int index = obj1.getLetterIndex( "RANE " + letters, 0);
+		int index = CharContainer.getLetterIndex( "RANE " + letters, 0);
 		assertEquals( 3, index);
-		index = obj1.getLetterIndex( "RANE " + letters, 1);
+		index = CharContainer.getLetterIndex( "RANE " + letters, 1);
 		assertEquals( 0, index);
-		index = obj1.getLetterIndex( "RANE " + letters, 2);
+		index = CharContainer.getLetterIndex( "RANE " + letters, 2);
 		assertEquals( 1, index);
-		index = obj1.getLetterIndex( "RANE " + letters, 3);
+		index = CharContainer.getLetterIndex( "RANE " + letters, 3);
 		assertEquals( 5, index);
-		index = obj1.getLetterIndex( "RANE " + letters, 4);
+		index = CharContainer.getLetterIndex( "RANE " + letters, 4);
 		assertEquals( CharContainer.NO_INDEX, index);
-		index = obj1.getLetterIndex( "RANE " + letters, 5);
+		index = CharContainer.getLetterIndex( "RANE " + letters, 5);
 		assertEquals( CharContainer.NO_INDEX, index);
-		index = obj1.getLetterIndex( "RANEERTRWE " + letters, 6);
+		index = CharContainer.getLetterIndex( "RANEERTRWE " + letters, 6);
 		assertEquals( CharContainer.NO_INDEX, index);
 
-		index = obj1.getLetterIndex( "RANE " + letters, -1);
+		index = CharContainer.getLetterIndex( "RANE " + letters, -1);
 		assertEquals( CharContainer.NO_INDEX, index);
 }
 
@@ -81,15 +81,15 @@ public class CharContainerTest02 {
 	public void test04() {
 		String letters = "ESWZE";
 		obj1.InitLetters(letters);
-		int index = obj1.getLetterIndex( "SZEWE " + letters, 0);
+		int index = CharContainer.getLetterIndex( "SZEWE " + letters, 0);
 		assertEquals( 1, index);
-		index = obj1.getLetterIndex( "SZEWE " + letters, 1);
+		index = CharContainer.getLetterIndex( "SZEWE " + letters, 1);
 		assertEquals( 3, index);
-		index = obj1.getLetterIndex( "SZEWE " + letters, 2);
+		index = CharContainer.getLetterIndex( "SZEWE " + letters, 2);
 		assertEquals( 0, index);
-		index = obj1.getLetterIndex( "SZEWE " + letters, 3);
+		index = CharContainer.getLetterIndex( "SZEWE " + letters, 3);
 		assertEquals( 2, index);
-		index = obj1.getLetterIndex( "SZEWE " + letters, 4);
+		index = CharContainer.getLetterIndex( "SZEWE " + letters, 4);
 		assertEquals( 4, index);
 	}
 
